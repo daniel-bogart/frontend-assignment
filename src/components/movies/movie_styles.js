00c79c2a598cf5590cf1c9a1fe9aebad6376be48
daniel-bogart <daniel.bogart17@gmail.com>
2021-11-01 23:50:11 -0700

    fixed css to be more respomove

--- src/components/movies/movie_styles.js
+++ src/components/movies/movie_styles.js
@@ -5,38 +5,54 @@ export const MovieContainer = styled.div`
   overflow-x: hidden;
   justify-content: center;
   align-items: center;
+  /* width: 100%; */
+  max-width: calc(1200px + 4rem);
+  margin: 0 auto;
+`;
+
+export const MovieWrapper = styled.div`
+  /* max-width: 1200px; */
+  /* padding: 0 2rem; */
+  width: 100%;
+  /* margin: 0 auto; */
 `;
 
 export const Poster = styled.img`
-  width: 300px;
+  /* width: 300px; */
+  width: 100%;
   height: 340px;
   object-fit: cover;
   object-position: center;
   transition: all .5s ease;
   border-radius: 8px 8px 0px 0px;
+  @media (max-width: 568px) {
+    height: 100vw;
+  }
 `;
 
 export const MovieIndexList = styled.ul`
-  position: absolute;
-  justify-content: center;
-  align-items: center;
-  margin-top: 24px;
-  width: 98.8vw;
-  max-width: 100vw;
+  /* position: absolute; */
+  /* justify-content: center; */
+  /* align-items: center; */
+  /* width: 98.8vw; */
+  /* max-width: 100vw; */
+  margin: 24px 2rem 0 2rem;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(18.75rem, 18.75rem));
+  /* grid-template-columns: repeat(auto-fill, minmax(18.75rem, 18.75rem)); */
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
   column-gap: 1.5em;
   row-gap: 1.5em;
-  column-count: 4;
+  /* column-count: 4; */
 `;
 
 export const MovieIndexListItem = styled.li`
+  width: 100%;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   border-radius: 8px;
   transition: all 0.5s ease;
-  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  box-shadow: 1px 2px 8px 2px rgba(0, 0, 0, 0.1);
   &:hover {
     cursor: zoom-in;
     filter: brightness(80%);
@@ -72,8 +88,7 @@ export const Rating = styled.div`
 
 export const Border = styled.div`
   border-bottom: 3px solid rgb(219, 219, 219);
-  margin-right: 80px;
-  margin-left: 80px;
+  margin: 0 2rem;
 `;
 
 export const Header = styled.div`
@@ -81,6 +96,5 @@ export const Header = styled.div`
   font-size: 28px;
   font-weight: 600;
   position: relative;
-  left: 10%;
-  margin:0 auto;
+  margin: 0px 2rem;
 `;
--- src/components/navbar/navbar_styles.js
+++ src/components/navbar/navbar_styles.js
@@ -1,17 +1,19 @@
 import styled from "styled-components";
 
 export const SearchContainer = styled.div`
-  position: relative;
-  left: 13.4%;
-  margin:0 auto;
+  /* position: relative; */
+  /* left: 13.4%; */
+  /* margin:0 auto; */
 `;
 
 export const NavContainer = styled.div`
+  /* width: 100%; */
   z-index: 12;
   position: -webkit-sticky;
   position: sticky;
   top: 0px;
   background: white;
+  padding: 0px 2rem;
   /* justify-content: space-between; */
 `;
 
@@ -19,15 +21,24 @@ export const NavWrapper = styled.div`
   align-items: center;
   height: 85px;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  /* width: 100%; */
+  max-width: 1200px;
+  margin: 0 auto;
+  @media (max-width: 496px) {
+    /* display: block; */
+    /* height: auto; */
+    /* padding: 1rem 0; */
+  }
 `;
 
 export const Logo = styled.img`
-  position: relative;
-  right: 16%;
-  margin:0 auto;
+
   &:hover {
     cursor: pointer;
+  };
+  @media (max-width: 496px) {
+    /* margin-bottom: 1rem; */
   };
 `;
 
@@ -36,9 +47,12 @@ export const SearchWrapper = styled.div`
   align-items: center;
   width: 16rem;
   border: solid 2px rgb(189, 189, 189);
-  justify-content: center;
+  /* justify-content: center; */
   &:focus-within {
     border: solid 2px rgb(0, 85, 255);
+  };
+  @media (max-width: 496px) {
+    flex: 1;
   }
 `;
 
